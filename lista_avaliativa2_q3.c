0b9c246c478b74ed26fbc8ed6bdf03c66f4f8108
Rafaela <142021465+RafaPrattes@users.noreply.github.com>
2023-11-07 23:27:23 -0300

    lista_avaliativa2_q3.c

--- lista_avaliativa2_q3.c
+++ lista_avaliativa2_q3.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
-void print_matrix(int matrix[4][4]) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("%4d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
+#include <string.h>
 
 int main() {
     int matrizA[4][4];
     int matrizB[4][4];
     int resultado[4][4];
-    int i;
-    int j;
-    char operacao[5];
+    char operacao[10];
 
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
@@ -33,18 +23,24 @@ int main() {
 
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            if (operacao[0] == 's') {
+            if (strcmp(operacao, "soma") == 0) { 
                 resultado[i][j] = matrizA[i][j] + matrizB[i][j];
-            } else if (operacao[0] == 'sub') {
+            } else if (strcmp(operacao, "sub") == 0) { 
                 resultado[i][j] = matrizA[i][j] - matrizB[i][j];
-            } else if (operacao[0] == 'm') {
-                resultado[i][j] = matrizA[i][j] * matrizB[i][j];
+            } else if (strcmp(operacao, "mult") == 0) { 
+                resultado[i][j] = 0;
+                for (int k = 0; k < 4; k++) {
+                    resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+                }
             }
         }
     }
-        printf("%4d", resultado[i][j]);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%4d", resultado[i][j]);
+        }
         printf("\n");
+    }
 
-print_matrix("resultado");
     return 0;
 }
